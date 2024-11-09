650d05a68a54a0d8c71f399ce2e31808152e8693
Christina Maria Mastralexi <xristinamst@gmail.com>
2024-11-09 14:42:13 +0000

    Logging setup

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import argparse
+import logging
+
+def setup_logging(log_file):
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s", 
+                        handlers=[logging.FileHandler(log_file), logging.StreamHandler()])
+
 
 parser = argparse.ArgumentParser(description="Folder Synchronization")
 parser.add_argument("source", type=str, help="Path to the source folder")
 parser.add_argument("replica", type=str, help="Path to the replica folder")
-parser.add_argument("interval", type=int, help="Synchronization interval in seconds")
+parser.add_argument("interval", type=int, help="Synchronization interval (in seconds)")
 parser.add_argument("log_file", type=str, help="Path to the log file")
 args = parser.parse_args()
 
 # Output test
 print("Source Folder:", args.source)
 print("Replica Folder:", args.replica)
-print("Interval:", args.interval)
+print("Sync Interval:", args.interval)
 print("Log File:", args.log_file)
+
+setup_logging(args.log_file)
+    
+# Test logging
+logging.info("Logging successful.")
