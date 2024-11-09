d1aba7fe992ed701caaea5aed773157784f36c20
Christina Maria Mastralexi <xristinamst@gmail.com>
2024-11-09 15:53:07 +0000

    Refactoring

--- main.py
+++ main.py
@@ -51,21 +51,14 @@ def periodic_synchronization(source, replica, interval):
         sync_folder(source, replica)
         time.sleep(interval)    
 
-parser = argparse.ArgumentParser(description="Folder Synchronization")
-parser.add_argument("source", type=str, help="Path to the source folder")
-parser.add_argument("replica", type=str, help="Path to the replica folder")
-parser.add_argument("interval", type=int, help="Synchronization interval (in seconds)")
-parser.add_argument("log_file", type=str, help="Path to the log file")
-args = parser.parse_args()
+if __name__ == "__main__":
 
-# Output test
-print("Source Folder:", args.source)
-print("Replica Folder:", args.replica)
-print("Sync Interval:", args.interval)
-print("Log File:", args.log_file)
+    parser = argparse.ArgumentParser(description="Folder Synchronization")
+    parser.add_argument("source", type=str, help="Path to the source folder")
+    parser.add_argument("replica", type=str, help="Path to the replica folder")
+    parser.add_argument("interval", type=int, help="Synchronization interval (in seconds)")
+    parser.add_argument("log_file", type=str, help="Path to the log file")
+    args = parser.parse_args()
 
-setup_logging(args.log_file)
-    
-# Test logging
-logging.info("Logging successful.")
-periodic_synchronization(args.source, args.replica, args.interval)
+    setup_logging(args.log_file)
+    periodic_synchronization(args.source, args.replica, args.interval)
